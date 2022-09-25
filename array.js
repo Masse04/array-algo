6187a7aa0c64c5750a507b70c632e59cd5a77865
Masse04 <massamba084@gmail.com>
2022-09-25 16:31:21 +0100

    2eme projet algorithme

--- array.js
+++ array.js
@@ -1,21 +1,4 @@
-function test (Array1, Array2){
-    var som = 0
-    for(let i = 0 ; i < Array1.length ; i++){
-        for (let j = 0 ; j < Array2.length ; j++){
-            if(Array1[i] == Array2[j])
-                som += Array1[i]
-            }
-    }
-    for(let i = 0 ; i < Array2.length-1 ; i++){
-        for (let j = 0 ; j < Array1.length-1 ; j++){
-            if(Array2[i] == Array1[j])
-                som += Array2[i]
-            }
-    }
-    return som
-}
-test ([5,7,9,34,3],[7,6,5,4,3])
-
+ fonction distincte
 function test (Array1, Array2){
     var som = 0
     for(let i = 0 ; i < Array1.length ; i++){
@@ -30,6 +13,26 @@ function test (Array1, Array2){
         for (let j = 0 ; j < Array1.length ; j++){
             if(Array2[i] == Array1[j])
             som -= Array2[i]
+            }
+    }
+    return som
+}
+test ([5,7,9,34,3],[7,6,5,4,3])
+
+
+fonction égalité
+function test (Array1, Array2){
+    var som = 0
+    for(let i = 0 ; i < Array1.length ; i++){
+        for (let j = 0 ; j < Array2.length ; j++){
+            if(Array1[i] == Array2[j])
+                som += Array1[i]
+            }
+    }
+    for(let i = 0 ; i < Array2.length ; i++){
+        for (let j = 0 ; j < Array1.length ; j++){
+            if(Array2[i] == Array1[j])
+                som += Array2[i]
             }
     }
     return som
