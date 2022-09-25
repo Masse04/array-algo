79ad9cab96c8e387c98c98f4176bb5c479c04d3e
Masse04 <massamba084@gmail.com>
2022-09-25 16:24:28 +0100

    2eme projet algo

--- array.js
+++ array.js
@@ -6,38 +6,32 @@ function test (Array1, Array2){
                 som += Array1[i]
             }
     }
-    for(let i = 0 ; i < Array2.length ; i++){
-        for (let j = 0 ; j < Array1.length ; j++){
+    for(let i = 0 ; i < Array2.length-1 ; i++){
+        for (let j = 0 ; j < Array1.length-1 ; j++){
             if(Array2[i] == Array1[j])
                 som += Array2[i]
             }
     }
     return som
 }
-test ([5,7,9,34],[7,6,5,4])
+test ([5,7,9,34,3],[7,6,5,4,3])
 
 function test (Array1, Array2){
     var som = 0
     for(let i = 0 ; i < Array1.length ; i++){
         som += Array1[i]
         for (let j = 0 ; j < Array2.length ; j++){
-            if(Array1[i] != Array2[j])
-                continue
-            else
+            if(Array1[i] == Array2[j])
                 som -= Array1[i]
-                console.log(som)
             }
     }
     for(let i = 0 ; i < Array2.length ; i++){
         som += Array2[i]
         for (let j = 0 ; j < Array1.length ; j++){
-            if(Array2[i] != Array1[j])
-                continue
-            else
-                som -= Array2[i]
-                console.log(som)
+            if(Array2[i] == Array1[j])
+            som -= Array2[i]
             }
     }
     return som
 }
-test ([5,7,9,34],[7,6,5,4])
+test ([5,7,9,34,3],[7,6,5,4,3])
